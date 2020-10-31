7355f2a7a00f078b19c783806e7ad1fdd7d906ce
FALCH2000 <faleiva.ch@gmail.com>
2020-10-31 00:04:45 -0600

    Inserción de clase Table_Model

--- Interface/table_model.cpp
+++ Interface/table_model.cpp
@@ -1,6 +1,116 @@
 #include "table_model.h"
 
-Table_Model::Table_Model()
+Table_Model::Table_Model(QObject *parent)
+    : QAbstractTableModel(parent)
 {
+}
+
+
+
+//-------------------------------------------------------
+int Table_Model::rowCount(const QModelIndex & /*parent*/) const
+{
+    return rowNumber;
+}
+
+//-------------------------------------------------------
+int Table_Model::columnCount(const QModelIndex & /*parent*/) const
+{
+    return 4;
+}
+
+/**
+ * @brief Table_Model::data This method assign the data to be displayed
+ * @param index it's a parameter of the method inherited
+ * @param role it's a parameter of the method inherited
+ * @return the data to be displayed in a qstring
+ */
+QVariant Table_Model::data(const QModelIndex &index, int role) const
+{
+
+    if (role == Qt::DisplayRole)
+    {
+        int x= index.row();
+        Song *ss= this->SongsList_Vector->at(x);
+
+        if(index.column()==0){
+            QString qstr= QString::fromStdString(ss->getId());
+            return qstr;
+        } else if(index.column()==1){
+            QString qstr= QString::fromStdString(ss->getName());
+            return qstr;
+        }else if(index.column()==2){
+            QString qstr= QString::fromStdString(ss->getArtist());
+            return qstr;
+        }else if(index.column()==3){
+            QString qstr= QString::fromStdString(ss->getLength());
+            return qstr;
+        }
+
+    }
+
+    return QVariant();
+}
+
+
+QVariant Table_Model::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole)
+    {
+        if (orientation == Qt::Horizontal) {
+            switch (section)
+            {
+            case 0:
+                return QString("Song ID");
+            case 1:
+                return QString("Song Name");
+            case 2:
+                return QString("Artist Name");
+            case 3:
+                return QString("Duration");
+
+            }
+        }
+    }
+    return QVariant();
+}
+/**
+ * @brief Table_Model::chargeSongs charge all songs in constructor
+ * @param start will mark the start of the songs to load in the constructor
+ * @param end will mark the end of the songs to load in the constructor
+ */
+void Table_Model::chargeSongs(int start, int end){
+
+    if(constructor==nullptr){
+        constructor= new Constructor("/home/fatima/Documents/tracks.csv", "Songs");
+        constructor->ParseCSV(start,end);
+
+    }else{
+        resetModel();
+        constructor->ParseCSV(start,end);
+    }
+
+    // ----------- SETEAR ESTE VECTOR ---------
+    SongsList_Vector = constructor->getSongsList();
+
+    rowNumber=this->SongsList_Vector->size();
+}
+
+QString Table_Model::getUrl(int position){
+    Song *ss= this->SongsList_Vector->at(position);
+    QString qstr= QString::fromStdString(ss->getUrl());
+    return qstr;
+}
+
+/**
+ * @brief Table_Model::SongList it's a signal. Emits when the data change
+ * @param VectorSize is the vector of the songs
+ * --------------------------- I should erase the method ---------------------------
+ */
+void Table_Model::resetModel(){
+    beginResetModel();
+    qDeleteAll(this->SongsList_Vector->begin(), this->SongsList_Vector->end());
+    SongsList_Vector->clear();
+    endResetModel();
 
 }
--- Interface/table_model.h
+++ Interface/table_model.h
@@ -2,10 +2,65 @@
 #define TABLE_MODEL_H
 
 
-class Table_Model
+#include <QAbstractTableModel>
+#include "constructor.h"
+#include "song.h"
+
+class Table_Model : public QAbstractTableModel
 {
+    Q_OBJECT
+
+    Constructor *constructor=nullptr; /**< Is the object constructor for being displayed in this table model */
 public:
-    Table_Model();
+    int rowNumber; /**< Int that saves the row number for being displayed in this table model */
+    vector<Song*> *SongsList_Vector; /**< Is the list of Songs for being displayed in the table model.
+                                          Is a pointer to the Songs List created from the constructor*/
+    Table_Model(QObject *parent= nullptr); /**< Is the constructor of the class */
+    /**
+     * @brief rowCount establish the number of rows for being displayed in the table model.
+     * @return an int of the number of rows.
+     */
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    /**
+     * @brief columnCount establish the number of columns for being displayed in the table model.
+     * For this project the column Number is 4 and it's constant.
+     * @return an int with the number of columns.
+     */
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+
+    /**
+     * @brief data is an inherited method. Is called to set all the data to be displayed in the table.
+     * @return the value to be display in a specific item. Will return a QString and if is invalid,
+     * return a QVariant.
+     */
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+    /**
+     * @brief headerData set the information to be displayed in the table header
+     * @return a QString of the data for being displayed in a column.
+     */
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
+    /**
+     * @brief chargeSongs load Songs from an csv path specific.
+     * @param start mark the start of the songs list for being displayed in the table.
+     * @param end mark the end of the songs list for being displayed in the table.
+     */
+    void chargeSongs(int start, int end);
+
+    /**
+     * @brief getUrl gets the url of a specific song that is in a specific row.
+     * @param position is the position of the row where the song is.
+     * @return the absolute path where the song is.
+     */
+    QString getUrl(int position);
+
+public slots:
+
+    /**
+     * @brief resetModel reset the table model. Allows to update the table over and over again.
+     */
+    void resetModel();
 };
 
 #endif // TABLE_MODEL_H
